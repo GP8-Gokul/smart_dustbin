b6b53f8d3dd2d91f62b8c4b3efd970c1413f8578
Gokul <156660981+GP8-Gokul@users.noreply.github.com>
2025-11-16 21:24:03 +0530

    V1

---
+++ smart_dustbin.ino
@@ -0,0 +1,107 @@
+// --- BLYNK DEFINITIONS ---
+#define BLYNK_TEMPLATE_ID "TMPL3SL77Uhy_"
+#define BLYNK_TEMPLATE_NAME "smart dustbin"
+#define BLYNK_AUTH_TOKEN "FjcrIRtO13N04iVCTta29k-rUWN1VYHk"
+#define BLYNK_PRINT Serial
+
+#include <Servo.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// --- PIN DEFINITIONS ---
+#define IR_SENSOR_PIN D7
+#define SERVO_PIN     D3
+#define TRIG_PIN      D1
+#define ECHO_PIN      D5
+#define BUZZER_PIN    D6
+
+// --- WIFI & BLYNK SETUP ---
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "test";
+char pass[] = "12341234";
+
+// --- GLOBAL OBJECTS ---
+Servo s1;
+BlynkTimer timer;
+long duration;
+int distance;
+
+// ===================================================================
+// SETUP
+// ===================================================================
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(IR_SENSOR_PIN, INPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+
+  s1.attach(SERVO_PIN);
+  s1.write(0); 
+
+  Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+
+  timer.setInterval(2000L, sendSensorData); 
+}
+
+// ===================================================================
+// LOOP
+// ===================================================================
+void loop() {
+  Blynk.run();
+  timer.run();
+  checkLid();
+}
+
+// ===================================================================
+// FUNCTION: checkLid
+// Opens lid when hand is detected
+// ===================================================================
+void checkLid() {
+  int irValue = digitalRead(IR_SENSOR_PIN);
+  if (irValue == LOW) { 
+    Serial.println("Hand Detected! Opening lid.");
+    s1.write(60);  
+    delay(3000);    
+    s1.write(0);    
+    delay(1000);    
+  }
+}
+
+
+// ===================================================================
+// FUNCTION: sendSensorData
+// Reads ultrasonic sensor and updates Blynk
+// ===================================================================
+bool binFullFlag = false;
+void sendSensorData() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(5);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(15);
+  digitalWrite(TRIG_PIN, LOW);
+
+  duration = pulseIn(ECHO_PIN, HIGH);
+  distance = duration * 0.034 / 2; 
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  Blynk.virtualWrite(V0, distance);
+
+  if (distance > 0 && distance < 8) {
+        Blynk.virtualWrite(V1, "Full"); 
+
+        if (!binFullFlag) { 
+            digitalWrite(BUZZER_PIN, HIGH);
+            delay(200);      
+            digitalWrite(BUZZER_PIN, LOW);
+            binFullFlag = true;
+        }
+    } else {
+        Blynk.virtualWrite(V1, String(distance) + " cm"); 
+        binFullFlag = false; 
+    }
+}
